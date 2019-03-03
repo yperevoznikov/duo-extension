2b6cc41f8dbeb5aa38da1c731d5c25038f0c235c
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 18:57:53 +0300

    fix webtask

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express  = require('express');
 var auth0    = require('auth0-oauth2-express');
-var Webtask  = require('webtask-tools');
 var app      = express();
-var metadata = require('./webtask.json');
+// var metadata = require('./webtask.json');
 
 app.use(auth0({
   scopes: 'read:connections'
@@ -31,14 +30,15 @@ app.get('/', function (req, res) {
     '  </body>',
     '</html>'
   ].join('\n');
+  var view = 'hello world';
 
   res.header("Content-Type", 'text/html');
   res.status(200).send(view);
 });
 
 // This endpoint would be called by webtask-gallery to dicover your metadata
-app.get('/meta', function (req, res) {
-  res.status(200).send(metadata);
-});
+// app.get('/meta', function (req, res) {
+//   res.status(200).send(metadata);
+// });
 
 module.exports = app;
