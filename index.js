bc8da60fdcdc44a845a2c6bc484db4c20f7c6cff
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 19:39:25 +0300

    remove all routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express  = require('express');
 var app      = express();
 
-app.get('/', function (req, res) {
-  var view = 'Hello world!';
-
-  res.header("Content-Type", 'text/html');
-  res.status(200).send(view);
-});
+// app.get('/', function (req, res) {
+//   var view = 'Hello world!';
+//
+//   res.header("Content-Type", 'text/html');
+//   res.status(200).send(view);
+// });
 
 module.exports = app;
