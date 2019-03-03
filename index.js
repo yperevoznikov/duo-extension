986dfa51c44f5e55774ab7327935fe8122da9fb0
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 20:32:47 +0300

    remove route for home page

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var express  = require('express');
 // var app      = express();
 var app = express.Router()
 
-app.get('/', function (req, res) {
-  res.status(200).send('Hello world!');
-});
+// app.get('/', function (req, res) {
+//   res.status(200).send('Hello world!');
+// });
 
 // app.listen(port, function () {
 //   console.log('Server started on port', port);
