7851050490fc73ac8d9138b9860ed4ee2e0aae9f
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 19:44:02 +0300

    simplify the router

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express  = require('express');
 var app      = express();
 
-// app.get('/', function (req, res) {
-app.get('/', function (app, passport, req, res, next) {
-  var view = 'Hello world!';
-
-  res.header("Content-Type", 'text/html');
-  res.status(200).send(view);
+app.get('/', function (req, res) {
+  res.status(200).send('Hello world!');
 });
 
 module.exports = app;
