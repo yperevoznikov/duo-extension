b0d2804b0e3b67d1ee90eadbdb00a2b6c17e90a0
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 20:06:28 +0300

    simplify the code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express  = require('express');
-var app      = express();
+// var express  = require('express');
+// var app      = express();
+//
+// app.get('/', function (req, res) {
+//   res.status(200).send('Hello world!');
+// });
+//
+// module.exports = app;
 
-app.get('/', function (req, res) {
-  res.status(200).send('Hello world!');
-});
-
-module.exports = app;
+module.exports = {};
