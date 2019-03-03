7a81a5563f665242738486166bdfdeda5b20c708
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 18:31:03 +0300

    clone extensions

--- tmp/server.js
+++ tmp/server.js
@@ -3,5 +3,5 @@ var App = require('./');
 var port = process.env.PORT || 3000;
 
 App.listen(port, function () {
-    console.log('Server started on port', port);
+  console.log('Server started on port', port);
 })
