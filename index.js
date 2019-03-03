24b4180000c1c3f34c391b6e7e46f7cd89cb649e
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 20:13:31 +0300

    make one file app

---
+++ index.js
@@ -0,0 +1,21 @@
+// var express  = require('express');
+// var app      = express();
+//
+// app.get('/', function (req, res) {
+//   res.status(200).send('Hello world!');
+// });
+//
+// module.exports = app;
+
+var express  = require('express');
+var app      = express();
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello world!');
+});
+
+var port = process.env.PORT || 80;
+
+App.listen(port, function () {
+  console.log('Server started on port', port);
+})
