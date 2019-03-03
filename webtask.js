94b1bef25bf620a3e227397d084ee526fb632bc0
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 20:07:50 +0300

    increment version, remove unneded files

--- webtask.js
+++ webtask.js
@@ -1,4 +1,4 @@
-var Webtask = require('webtask-tools');
+// var Webtask = require('webtask-tools');
 
 // This is the entry-point for the Webpack build. We need to convert our module
 // (which is a simple Express server) into a Webtask-compatible function.
