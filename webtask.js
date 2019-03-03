72dbf78d14f4d17b5e575f379307c8496001c20b
Yuriy Perevoznikov <yuriless@gmail.com>
2019-03-03 19:31:13 +0300

    make webtask super simple

--- webtask.js
+++ webtask.js
@@ -2,4 +2,6 @@ var Webtask = require('webtask-tools');
 
 // This is the entry-point for the Webpack build. We need to convert our module
 // (which is a simple Express server) into a Webtask-compatible function.
-module.exports = Webtask.fromExpress(require('./index.js'));
+// module.exports = Webtask.fromExpress(require('./index.js'));
+
+module.exports = function(cb) { cb(null, 'hello world'); }
